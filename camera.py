73b966f5a717b22ffa51b174d2202e88b40d6b49
Jordan Williams <jordan@jwillikers.com>
2024-01-21 13:03:31 -0600

    Install wlr-randr and missing dep and make fixes

--- camera.py
+++ camera.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
+import time
+
 import board
-from smbus2 import SMBus
 from libcamera import controls, Transform
 import adafruit_pcf8574
 from picamera2 import Picamera2, Preview
-import time
-import busio
 import adafruit_bitbangio
 import digitalio
 
@@ -20,16 +19,17 @@ with Picamera2() as picam2:
 
     # Set up QT preview window.
     preview_config = picam2.create_preview_configuration()
-    # preview_config["transform"] = Transform(rotation=270)
-    #preview_config["transform"] = Transform(rotation=1)
+    # Only rotating 180 degrees is supported...
+    # preview_config["transform"] = Transform(hflip=True, vflip=True)
     capture_config = picam2.create_still_configuration()
+    capture_config["transform"] = Transform(rotation=270)
     picam2.configure(preview_config)
     picam2.start_preview(Preview.QTGL, x=0, y=0, width=800, height=480)
     picam2.start()
     time.sleep(2)
 
     # Turn on full-time autofocus.
-    picam2.set_controls({"AfMode": 2 ,"AfTrigger": 0})
+    picam2.set_controls({"AfMode": controls.AfModeEnum.Continuous})
 
     time.sleep(1)
     print("Preview started")
@@ -39,9 +39,9 @@ with Picamera2() as picam2:
         cur_state = button.value
         if cur_state != prev_state:
             if not cur_state:
-                filename = '/home/jordan/Pictures/%03d.jpg' % frame
+                filename = f"/home/jordan/Pictures/{frame}.jpg"
                 picam2.switch_mode_and_capture_file(capture_config, filename)
-                print('Image captured: ' + filename)
+                print(f"Image captured: {filename}")
                 frame += 1
             else:
                 print("BTN is up")
