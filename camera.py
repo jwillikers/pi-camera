828e0a0496b987d95b7f03953028d65d469bf2b1
Jordan Williams <jordan@jwillikers.com>
2024-02-23 17:39:58 -0600

    Add an I2C GPS to encode geolocation metadata in the photos

--- camera.py
+++ camera.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import time
 
+import io
+import date
 import board
 from libcamera import controls
 import adafruit_pcf8574
@@ -8,11 +10,22 @@ from picamera2 import Picamera2, Preview
 import adafruit_bitbangio
 import digitalio
 
+from exif import Image
+from exif import GpsAltitudeRef
+
+import adafruit_gps
+
+
 i2c = adafruit_bitbangio.I2C(board.D11, board.D10)
 
 pcf = adafruit_pcf8574.PCF8574(i2c)
 button = pcf.get_pin(0)
 button.switch_to_input(pull=digitalio.Pull.UP)
+
+# Initialize the GPS
+gps = adafruit_gps.GPS_GtopI2C(i2c, debug=False)
+gps.send_command(b"PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0")
+gps.send_command(b"PMTK220,1000")
 
 with Picamera2() as picam2:
     frame = int(time.time())
@@ -39,7 +52,36 @@ with Picamera2() as picam2:
         if cur_state != prev_state:
             if not cur_state:
                 filename = f"/home/jordan/Pictures/{frame}.jpg"
-                picam2.switch_mode_and_capture_file(capture_config, filename)
+                data = io.BytesIO()
+                picam2.switch_mode_and_capture_file(capture_config, data)
+                image = Image(data)
+                if gps.has_fix:
+                    # https://exiftool.org/TagNames/GPS.html
+                    # https://exif.readthedocs.io/en/latest/api_reference.html#image
+                    # todo Needed?
+                    # image.gps_version_id = (2, 3, 0, 0)
+                    image.gps_latitude = gps.latitude
+                    image.gps_latitude_ref = "N" if gps.latitude > 0 else "S"
+                    image.gps_longitude = gps.longitude
+                    image.gps_longitude_ref = "E" if gps.longitude > 0 else "W"
+                    image.gps_altitude = gps.altitude_m
+                    image.gps_altitude_ref = (
+                        GpsAltitudeRef.ABOVE_SEA_LEVEL
+                        if gps.altitude_m > 0
+                        else GpsAltitudeRef.BELOW_SEA_LEVEL
+                    )
+                    image.gps_datestamp = date.fromtimestamp(gps.timestamp_utc)
+                    image.gps_timestamp = gps.timestamp_utc
+                    image.gps_satellites = gps.satellites
+                    image.gps_status = gps.isactivedata
+                    image.gps_measure_mode = gps.fix_quality_3d
+                    image.gps_speed_ref = "N"  # knots
+                    image.gps_speed = gps.speed_knots
+                    image.gps_processing_method = "GPS"
+                    image.gps_track = gps.track_angle_deg
+                    # todo image.gps_track_ref = "M" or "T"?
+                with open(filename, "wb") as new_image_file:
+                    new_image_file.write(image.get_file())
                 print(f"Image captured: {filename}")
                 frame += 1
             else:
