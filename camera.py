d51d17dbfc393640c2a122aa8fe6944efb9e8384
Jordan Williams <jordan@jwillikers.com>
2024-02-24 14:45:51 -0600

    Use piexif to for exif metadata

--- camera.py
+++ camera.py
@@ -1,90 +1,179 @@
 #!/usr/bin/env python3
+import argparse
+from fractions import Fraction
+import os
+import signal
+import sys
 import time
 
-import io
-import date
-import board
-from libcamera import controls
-import adafruit_pcf8574
-from picamera2 import Picamera2, Preview
-import adafruit_bitbangio
-import digitalio
-
-from exif import Image
-from exif import GpsAltitudeRef
-
 import adafruit_gps
+import adafruit_pcf8574
+import adafruit_bitbangio
+import board
+import digitalio
+from libcamera import controls
+from picamera2 import Picamera2, Preview
+import piexif
 
 
-i2c = adafruit_bitbangio.I2C(board.D11, board.D10)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--output", help="Directory path for the output images.")
+    args = parser.parse_args()
 
-pcf = adafruit_pcf8574.PCF8574(i2c)
-button = pcf.get_pin(0)
-button.switch_to_input(pull=digitalio.Pull.UP)
+    output_directory = os.path.join(os.getenv("HOME"), "Pictures")
+    if args.output:
+        output_directory = args.output
+    if not os.path.isdir(output_directory):
+        print(f"The output directory '{output_directory}' does not exist")
+        print(f"Creating the output directory '{output_directory}'")
+        try:
+            os.mkdir(output_directory)
+        except FileExistsError:
+            pass
 
-# Initialize the GPS
-gps = adafruit_gps.GPS_GtopI2C(i2c, debug=False)
-gps.send_command(b"PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0")
-gps.send_command(b"PMTK220,1000")
+    i2c = adafruit_bitbangio.I2C(board.D11, board.D10)
 
-with Picamera2() as picam2:
+    pcf = adafruit_pcf8574.PCF8574(i2c)
+    button = pcf.get_pin(0)
+    button.switch_to_input(pull=digitalio.Pull.UP)
+
+    # Initialize the GPS
+    gps = adafruit_gps.GPS_GtopI2C(i2c, debug=False)
+    gps.send_command(b"PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0")
+    gps.send_command(b"PMTK220,1000")
+
     frame = int(time.time())
+    with Picamera2() as picam2:
+        picam2.options["quality"] = 95
+        picam2.options["compress_level"] = 9
 
-    # Set up QT preview window.
-    preview_config = picam2.create_preview_configuration()
-    # Only rotating 180 degrees is supported...
-    # preview_config["transform"] = Transform(hflip=True, vflip=True)
-    capture_config = picam2.create_still_configuration()
-    picam2.configure(preview_config)
-    picam2.start_preview(Preview.QTGL, x=0, y=0, width=800, height=480)
-    picam2.start()
-    time.sleep(2)
+        # Set up QT preview window.
+        preview_config = picam2.create_preview_configuration()
+        # Only rotating 180 degrees is supported...
+        # preview_config["transform"] = Transform(hflip=True, vflip=True)
+        capture_config = picam2.create_still_configuration()
+        picam2.configure(preview_config)
+        picam2.start_preview(Preview.QTGL, x=0, y=0, width=800, height=480)
+        picam2.start()
 
-    # Turn on full-time autofocus.
-    picam2.set_controls({"AfMode": controls.AfModeEnum.Continuous})
+        def signal_handler(_sig, _frame):
+            print("You pressed Ctrl+C!")
+            picam2.stop()
+            sys.exit(0)
 
-    time.sleep(1)
-    print("Preview started")
+        signal.signal(signal.SIGINT, signal_handler)
 
-    prev_state = button.value
-    while True:
-        cur_state = button.value
-        if cur_state != prev_state:
-            if not cur_state:
-                filename = f"/home/jordan/Pictures/{frame}.jpg"
-                data = io.BytesIO()
-                picam2.switch_mode_and_capture_file(capture_config, data)
-                image = Image(data)
-                if gps.has_fix:
-                    # https://exiftool.org/TagNames/GPS.html
-                    # https://exif.readthedocs.io/en/latest/api_reference.html#image
-                    # todo Needed?
-                    # image.gps_version_id = (2, 3, 0, 0)
-                    image.gps_latitude = gps.latitude
-                    image.gps_latitude_ref = "N" if gps.latitude > 0 else "S"
-                    image.gps_longitude = gps.longitude
-                    image.gps_longitude_ref = "E" if gps.longitude > 0 else "W"
-                    image.gps_altitude = gps.altitude_m
-                    image.gps_altitude_ref = (
-                        GpsAltitudeRef.ABOVE_SEA_LEVEL
-                        if gps.altitude_m > 0
-                        else GpsAltitudeRef.BELOW_SEA_LEVEL
+        time.sleep(2)
+
+        # Turn on full-time autofocus.
+        picam2.set_controls({"AfMode": controls.AfModeEnum.Continuous})
+
+        time.sleep(1)
+        print("Preview started")
+
+        prev_state = button.value
+        while True:
+            cur_state = button.value
+            if cur_state != prev_state:
+                if not cur_state:
+                    exif_dict = {}
+                    if gps.update() and gps.has_fix:
+
+                        def decimal_minutes_to_minutes_seconds(
+                            minutes_decimal: float,
+                        ) -> tuple[int, Fraction]:
+                            minutes = int(minutes_decimal)
+                            seconds = Fraction(
+                                (minutes_decimal - minutes) * 60
+                            ).limit_denominator(100)
+                            return (minutes, seconds)
+
+                        latitude_minutes, latitude_seconds = (
+                            decimal_minutes_to_minutes_seconds(gps.latitude_minutes)
+                        )
+                        longitude_minutes, longitude_seconds = (
+                            decimal_minutes_to_minutes_seconds(gps.longitude_minutes)
+                        )
+
+                        def format_fraction(fraction: Fraction) -> tuple[int, int]:
+                            return (
+                                int(fraction.limit_denominator(100).numerator),
+                                int(fraction.limit_denominator(100).denominator),
+                            )
+
+                        gps_ifd = {
+                            piexif.GPSIFD.GPSVersionID: (2, 3, 0, 0),
+                            piexif.GPSIFD.GPSAltitude: format_fraction(
+                                Fraction(
+                                    abs(0 if gps.altitude_m is None else gps.altitude_m)
+                                ).limit_denominator(100)
+                            ),
+                            piexif.GPSIFD.GPSAltitudeRef: (
+                                0 if gps.altitude_m is None or gps.altitude_m > 0 else 1
+                            ),
+                            piexif.GPSIFD.GPSLatitude: (
+                                (abs(gps.latitude_degrees), 1),
+                                (latitude_minutes, 1),
+                                format_fraction(latitude_seconds),
+                            ),
+                            piexif.GPSIFD.GPSLatitudeRef: (
+                                "N" if gps.latitude > 0 else "S"
+                            ),
+                            piexif.GPSIFD.GPSLongitude: (
+                                (abs(gps.longitude_degrees), 1),
+                                (longitude_minutes, 1),
+                                format_fraction(longitude_seconds),
+                            ),
+                            piexif.GPSIFD.GPSLongitudeRef: (
+                                "E" if gps.longitude > 0 else "W"
+                            ),
+                            piexif.GPSIFD.GPSProcessingMethod: "GPS".encode("ASCII"),
+                            piexif.GPSIFD.GPSSatellites: str(gps.satellites),
+                            piexif.GPSIFD.GPSSpeed: (
+                                (0, 1)
+                                if gps.speed_knots is None
+                                else format_fraction(Fraction(gps.speed_knots))
+                            ),
+                            piexif.GPSIFD.GPSSpeedRef: "N",
+                        }
+                        if gps.fix_quality_3d > 0:
+                            gps_ifd[piexif.GPSIFD.GPSMeasureMode] = str(
+                                gps.fix_quality_3d
+                            )
+                        if gps.timestamp_utc:
+                            gps_ifd[piexif.GPSIFD.GPSDateStamp] = time.strftime(
+                                "%Y:%m:%d", gps.timestamp_utc
+                            )
+                            gps_ifd[piexif.GPSIFD.GPSTimeStamp] = (
+                                (gps.timestamp_utc.tm_hour, 1),
+                                (gps.timestamp_utc.tm_min, 1),
+                                (gps.timestamp_utc.tm_sec, 1),
+                            )
+                        if gps.isactivedata:
+                            gps_ifd[piexif.GPSIFD.GPSStatus] = gps.isactivedata
+                        exif_dict = {"GPS": gps_ifd}
+                        print(f"Exif GPS metadata: {gps_ifd}")
+                    else:
+                        print("No GPS fix")
+                    filename = os.path.join(output_directory, f"{frame}.jpg")
+                    capture_config = picam2.create_still_configuration()
+                    picam2.switch_mode_and_capture_file(
+                        capture_config,
+                        filename,
+                        delay=10,
+                        exif_data=exif_dict,
+                        format="jpeg",
                     )
-                    image.gps_datestamp = date.fromtimestamp(gps.timestamp_utc)
-                    image.gps_timestamp = gps.timestamp_utc
-                    image.gps_satellites = gps.satellites
-                    image.gps_status = gps.isactivedata
-                    image.gps_measure_mode = gps.fix_quality_3d
-                    image.gps_speed_ref = "N"  # knots
-                    image.gps_speed = gps.speed_knots
-                    image.gps_processing_method = "GPS"
-                    image.gps_track = gps.track_angle_deg
-                    # todo image.gps_track_ref = "M" or "T"?
-                with open(filename, "wb") as new_image_file:
-                    new_image_file.write(image.get_file())
-                print(f"Image captured: {filename}")
-                frame += 1
-            else:
-                print("BTN is up")
-        prev_state = cur_state
-        time.sleep(0.01)  # debounce
+                    print(f"Image captured: {filename}")
+                    frame += 1
+                else:
+                    print("BTN is up")
+            prev_state = cur_state
+            gps.update()
+            # debounce
+            time.sleep(0.01)
+
+
+if __name__ == "__main__":
+    main()
